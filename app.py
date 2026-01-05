c67631fb7d737dafc4a8a83d5c47004913d4e3ca
talashtests <talashtests@gmail.com>
2026-01-05 11:51:12 +0200

    update greeting message in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from your CI/CD K3S Cluster!"
+    return "Hello from your CI/CD K3S Cluster!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
