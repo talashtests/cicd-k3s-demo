ce67b395a08687be7bc720bce778ede6b6cb77db
talashtests <talashtests@gmail.com>
2026-02-25 19:48:38 +0200

    Change greeting message in Flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from your CI/CD K3S Cluster!!"
+    return "GitOps is working!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
